448f6f62df49c08f2062542b89e6fae8771af6c8
nariskim <rhdee@naver.com>
2022-05-16 22:50:16 +0900

    .

--- src/main/java/com/julyte/user/modules/member/MemberController.java
+++ src/main/java/com/julyte/user/modules/member/MemberController.java
@@ -1,5 +1,9 @@
 package com.julyte.user.modules.member;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
@@ -14,17 +18,109 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
- 
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.julyte.user.common.constants.Constants;
 import com.julyte.user.common.util.UtilDateTime;
 import com.julyte.user.modules.code.CodeServiceImpl;
 
 @Controller
 public class MemberController {
- 
+
 	@Autowired
 	MemberServiceImpl service;
 
+	@RequestMapping(value = "/member/memberView2") // 브라우저창에 입력하는 주소
+	public String memberView2(Model model) throws Exception {
+
+		// api 호출해서 데이터를 받아 온다.
+
+		String apiUrl = "http://localhost:8080/rest/member/4342";
+
+		URL url = new URL(apiUrl);
+		HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+		httpURLConnection.setRequestMethod("GET");
+
+		BufferedReader bufferedReader;
+		if (httpURLConnection.getResponseCode() >= 200 && httpURLConnection.getResponseCode() <= 300) {
+			bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+		} else {
+			bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getErrorStream()));
+		}
+
+		StringBuilder stringBuilder = new StringBuilder();
+		String line;
+		while ((line = bufferedReader.readLine()) != null) {
+			System.out.println("line: " + line);
+			stringBuilder.append(line);
+		}
+
+		bufferedReader.close();
+		httpURLConnection.disconnect();
+
+		System.out.println("final line:" + stringBuilder.append(line));
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		Member member = objectMapper.readValue(stringBuilder.toString(), Member.class);
+
+		model.addAttribute("item", member);
+
+		// json Object
+		// json String
+
+		// json object -> 객체로 변환
+		// json string -> 객체로 변환
+
+		return "/member/memberView2";
+	}
+	
+	@RequestMapping(value = "/member/memberList2") // 브라우저창에 입력하는 주소
+	public String memberList2(Model model) throws Exception {
+		
+//		api 호출해서 값을 가져온다.
+		String apiUrl = "http://localhost:8080/rest/member";
+		
+		URL url = new URL(apiUrl);
+		HttpURLConnection httpURLConnection =(HttpURLConnection) url.openConnection();
+		httpURLConnection.setRequestMethod("GET");
+		
+		BufferedReader bufferedReader;
+		if(httpURLConnection.getResponseCode() >= 200 && httpURLConnection.getResponseCode() <=300) {
+			bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+		}else {
+			bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+		}
+		
+		StringBuilder stringBuilder = new StringBuilder();
+		String line;
+		while((line = bufferedReader.readLine()) != null) {
+			System.out.println("line : "+line);
+			stringBuilder.append(line);
+		}
+		
+		bufferedReader.close();
+		httpURLConnection.disconnect();
+		
+		System.out.println("final line : "+stringBuilder.append(line));
+		
+		ObjectMapper objectMapper = new ObjectMapper();
+		List<Member> memberList = objectMapper.readValue(stringBuilder.toString(), new TypeReference<List<Member>>() {});
+		
+		model.addAttribute("list", memberList);
+//			json Object
+//				json String, json객체가 아니다
+		
+//			json Object -> 객체로 변환 : convert
+//			json String -> 객체로 변환
+		
+//		model 객체를 이용해서 jsp로 데이터를 넘겨준다
+//			객체:Member
+		
+		return "/member/memberList2";
+	}
+
+	/* project */
 	@RequestMapping(value = "/member/memberList")
 	public String durianList(@ModelAttribute("vo") MemberVo vo, Model model) throws Exception {
 
@@ -134,7 +230,8 @@ public class MemberController {
 	}
 
 	@RequestMapping(value = "/member/memberUpdt")
-	public String durianUpdt(@ModelAttribute("vo") Member dto, MemberVo vo, Model model, RedirectAttributes redirectAttributes) throws Exception {
+	public String durianUpdt(@ModelAttribute("vo") Member dto, MemberVo vo, Model model,
+			RedirectAttributes redirectAttributes) throws Exception {
 
 		service.update(dto);
 
---
+++ src/main/webapp/WEB-INF/views/member/memberList2.jsp
@@ -0,0 +1,31 @@
+<%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8" %>
+
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>    
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
+<%@ taglib prefix="rb" uri="http://www.springframework.org/tags" %>
+
+
+<!doctype html>
+<html lang="ko">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Bootstrap CSS -->
+<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+ -->
+
+
+</head>
+<body>
+<c:forEach items="${list}" var="item" varStatus="status">	
+	<c:out value="${item.oymbSeq}"/>
+	<c:out value="${item.oymbName}"/>
+	<c:out value="${item.oymbId}"/>
+	<br>
+</c:forEach>
+
+
+</body>
+</html>
